cb90c960304ee7ea122b808054ec8348667df025
truonggiangit793 <truonggiangit793@gmail.com>
2025-05-10 08:29:26 +0700

    init project

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/random',
+      },
+    ],
+  },
+  output: 'standalone',
+};
+
+module.exports = nextConfig;
