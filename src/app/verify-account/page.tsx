f419e3a2692f05a9f0832f5613379203ab52b3d3
Giang Pham Truong <truonggiangit793@gmail.com>
2025-11-19 09:45:35 +0700

    feat: update verify-account email template for improved personalization and consistency

--- src/app/verify-account/page.tsx
+++ src/app/verify-account/page.tsx
@@ -1,9 +1,8 @@
 import { CSSProperties } from 'react';
-import { Container, Img, Link, Section, Text } from '@react-email/components';
+import { Container, Img, Section, Text } from '@react-email/components';
 import { Tailwind } from '@react-email/tailwind';
 
 import Button from '@/app/_components/Button';
-import TemplateFooter from '@/app/_components/TemplateFooter';
 
 const containerStyle: CSSProperties = { maxWidth: '50em' };
 
@@ -11,8 +10,8 @@ export default function Page() {
   return (
     <Tailwind>
       {/* Header with Logo and Company Name */}
-      <Container style={containerStyle} className='px-4 pt-10 pb-6'>
-        <Img height='60' src={'/logo-text.png'} alt='Logo' />
+      <Container style={containerStyle} className='px-3 pt-10 pb-6'>
+        <Img height='30' src={'https://dev-sme.igap.vn/go-igap-logo.png'} alt='Logo' />
       </Container>
 
       {/* Main Content */}
@@ -20,17 +19,18 @@ export default function Page() {
         <Text className='text-gray-600 text-lg font-semibold'>Xác Thực Tài Khoản</Text>
 
         <Text className='text-gray-600'>
-          Xin chào <b>{'${requestData.userName}'}</b>,
+          Xin chào <b>{'${requestData.fullName}'}</b>,
         </Text>
 
         <Text className='text-gray-600'>
-          Cảm ơn bạn đã đăng ký tài khoản tại iGap. Để hoàn tất quá trình đăng ký và bắt đầu sử dụng
-          tài khoản, vui lòng nhấn vào nút bên dưới để xác thực địa chỉ email của bạn.
+          Cảm ơn bạn đã đăng ký tài khoản tại GO iGap. Để hoàn tất quá trình đăng ký và bắt đầu sử
+          dụng, vui lòng nhấn vào nút bên dưới để xác thực địa chỉ email và kích hoạt tài khoản của
+          bạn.
         </Text>
 
-        <Section className='flex justify-center'>
-          <Button href={'${approveUrl}'} variant='primary' className='ml-2'>
-            Xác Thực Tài Khoản
+        <Section className='flex justify-center pb-6'>
+          <Button href={'${confirmationLink}'} variant='primary'>
+            Kích Hoạt Tài Khoản
           </Button>
         </Section>
 
@@ -39,20 +39,11 @@ export default function Page() {
             <b>Lưu ý quan trọng:</b>
           </Text>
           <Text className='m-0 text-blue-700'>
-            - Link xác thực này chỉ có hiệu lực trong vòng <b>{'${requestData.dueDate}'}</b> kể từ
-            khi bạn nhận được email này.
+            - Liên kết xác thực này chỉ có hiệu lực trong vòng <b>{'${requestData.dueDays}'} giờ</b>{' '}
+            kể từ khi bạn nhận được email này.
           </Text>
           <Text className='m-0 text-blue-700'>
-            - Nếu bạn không thực hiện yêu cầu này, vui lòng bỏ qua email này.
-          </Text>
-          <Text className='m-0 text-blue-700'>
-            - Nếu cần hỗ trợ, vui lòng liên hệ:
-            <Link
-              className='ml-1 font-medium text-red-600'
-              href={'mailto:${requestData.supportEmail}'}
-            >
-              {'${requestData.supportEmail}'}
-            </Link>
+            - Vui lòng bỏ qua email nếu bạn không thực hiện yêu cầu này.
           </Text>
         </Container>
 
@@ -71,18 +62,15 @@ export default function Page() {
         </ul>
 
         <Text className='text-gray-600'>
-          Đây là email được gửi tự động từ hệ thống. Vui lòng không phản hồi trực tiếp email này.
-          Nếu cần hỗ trợ hoặc có bất kỳ thắc mắc nào, xin vui lòng liên hệ với bộ phận hỗ trợ.
+          Đây là email được gửi tự động từ hệ thống, vui lòng không phản hồi trực tiếp email này.
+          Nếu có bất kỳ thắc mắc nào, xin vui lòng liên hệ với bộ phận hỗ trợ của GO iGAP.
         </Text>
 
         <Section className='mb-6'>
           <Text className='text-gray-600 m-0'>Trân trọng,</Text>
-          <Text className='text-gray-800 m-0 font-semibold'>iGap SME Security Team</Text>
+          <Text className='text-gray-800 m-0 font-semibold'>GO iGAP</Text>
         </Section>
       </Container>
-
-      {/* Footer */}
-      <TemplateFooter />
     </Tailwind>
   );
 }
