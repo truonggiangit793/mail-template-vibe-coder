084131d34087c3e50c2a7ed48e889ae416913e25
Giang Pham Truong <truonggiangit793@gmail.com>
2025-11-19 09:54:35 +0700

    feat: update change-password email template for improved personalization and security recommendations

--- src/app/change-password/page.tsx
+++ src/app/change-password/page.tsx
@@ -1,8 +1,6 @@
 import { CSSProperties } from 'react';
-import { Container, Img, Link, Section, Text } from '@react-email/components';
+import { Container, Img, Section, Text } from '@react-email/components';
 import { Tailwind } from '@react-email/tailwind';
-
-import TemplateFooter from '@/app/_components/TemplateFooter';
 
 const containerStyle: CSSProperties = { maxWidth: '50em' };
 
@@ -11,7 +9,7 @@ export default function Page() {
     <Tailwind>
       {/* Header with Logo and Company Name */}
       <Container style={containerStyle} className='px-4 pt-10 pb-6'>
-        <Img height='60' src={'/logo-text.png'} alt='Logo' />
+        <Img height='30' src={'https://dev-sme.igap.vn/go-igap-logo.png'} alt='Logo' />
       </Container>
 
       {/* Main Content */}
@@ -19,38 +17,13 @@ export default function Page() {
         <Text className='text-gray-600 text-lg font-semibold'>Xác Nhận Thay Đổi Mật Khẩu</Text>
 
         <Text className='text-gray-600'>
-          Xin chào <b>{'${requestData.userName}'}</b>,
+          Xin chào <b>{'${requestData.fullName}'}</b>,
         </Text>
 
         <Text className='text-gray-600'>
-          Chúng tôi gửi email này để xác nhận rằng mật khẩu tài khoản của bạn đã được thay đổi thành
-          công vào lúc <b>{'${requestData.changeDateTime}'}</b>.
+          Chúng tôi gửi email này để xác nhận rằng mật khẩu tài khoản GO iGAP của bạn đã được thay
+          đổi thành công vào lúc <b>{'${requestData.changeDateTime}'}</b>.
         </Text>
-
-        <Container className='bg-blue-50 rounded-lg p-6'>
-          <Text className='m-0 mb-2 text-blue-800 font-medium'>
-            <b>Thông Báo Bảo Mật Quan Trọng:</b> Nếu bạn không phải là người thực hiện thay đổi này,
-            vui lòng thực hiện ngay các bước sau:
-          </Text>
-          <Text className='m-0 text-blue-700'>
-            - Liên hệ đội ngũ bảo mật của chúng tôi qua email:
-            <Link
-              className='ml-1 font-medium text-red-600'
-              href={'mailto:${requestData.supportEmail}'}
-            >
-              {'${requestData.supportEmail}'}
-            </Link>
-          </Text>
-          <Text className='m-0 text-blue-700'>
-            - Gọi đường dây hỗ trợ 24/7:
-            <Link
-              className='ml-1 font-medium text-red-600'
-              href={'tel:${requestData.supportPhone}'}
-            >
-              {'${requestData.supportPhone}'}
-            </Link>
-          </Text>
-        </Container>
 
         <Text className='text-gray-600'>
           Để đảm bảo tính bảo mật cho tài khoản của bạn, chúng tôi khuyến nghị:
@@ -63,23 +36,33 @@ export default function Page() {
             </Text>
           </li>
           <li>
-            <Text className='m-0 text-gray-600'>Kích hoạt xác thực hai lớp.</Text>
+            <Text className='m-0 text-gray-600'>
+              Sử dụng mật khẩu mạnh và duy nhất cho tài khoản của bạn.
+            </Text>
+          </li>
+          <li>
+            <Text className='m-0 text-gray-600'>Không chia sẻ mật khẩu với bất kỳ ai.</Text>
+          </li>
+          <li>
+            <Text className='m-0 text-gray-600'>Đổi mật khẩu định kỳ để tăng cường bảo mật.</Text>
           </li>
           <li>
             <Text className='m-0 text-gray-600'>
-              Cập nhật mật khẩu tương tự trên các tài khoản khác.
+              Đảm bảo đăng xuất khỏi tài khoản trên các thiết bị công cộng hoặc lạ.
             </Text>
           </li>
         </ul>
 
+        <Text className='text-gray-600'>
+          Đây là email được gửi tự động từ hệ thống, vui lòng không phản hồi trực tiếp email này.
+          Nếu có bất kỳ thắc mắc nào, xin vui lòng liên hệ với bộ phận hỗ trợ của GO iGAP.
+        </Text>
+
         <Section className='mb-6'>
           <Text className='text-gray-600 m-0'>Trân trọng,</Text>
-          <Text className='text-gray-800 m-0 font-semibold'>iGap SME Security Team</Text>
+          <Text className='text-gray-800 m-0 font-semibold'>GO iGAP</Text>
         </Section>
       </Container>
-
-      {/* Footer */}
-      <TemplateFooter />
     </Tailwind>
   );
 }
